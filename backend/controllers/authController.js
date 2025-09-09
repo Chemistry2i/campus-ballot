3ef115858023ce3c63a5652eed7065c4cc1aeaa3
Wambogo Hassan Sadat <wambogohassan63@gmail.com>
2025-09-09 19:28:57 +0300

    Update authController.js
    Added login authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -99,8 +99,13 @@ const login = asyncHandler(async (req, res) => {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email }).select('+password');
+
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        if (!user.isVerified) {
+            return res.status(403).json({ message: 'Please verify your email before logging in.' });
         }
 
         user.lastLogin = new Date();
