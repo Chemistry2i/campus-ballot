40f526a3f2ccf1318059cf2927379868dca2103e
WAMBOGO  HASSAN SADAT <wambogohassan63@gmail.com>
2026-01-19 13:31:21 +0000

    UPdated super admin routes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -82,7 +82,7 @@ const LandingPage = () => {
 
       // Send to backend API
       const response = await fetch(
-        "https://legendary-space-journey-74p9qrwrq99hpppj-5000.app.github.dev/api/contact",
+        "https://api.campusballot.tech/api/contact",
         {
           method: "POST",
           headers: {
